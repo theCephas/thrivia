75f38c1c3713b5c594dc2340364975af1c298075
VictorIdowu <idsonvictor@gmail.com>
2024-08-27 23:26:21 +0100

    finance

--- FE/app/(root)/(tabs)/finance.tsx
+++ FE/app/(root)/(tabs)/finance.tsx
@@ -1,3 +1,4 @@
+import ArrowBack from "@/assets/svg/ArrowBack";
 import ArrowDown from "@/assets/svg/ArrowDown";
 import BgStyling from "@/assets/svg/BgStyling";
 import Unsee from "@/assets/svg/Unsee";
@@ -10,15 +11,30 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 const Finance = () => {
   const [activeNav, setActiveNav] = useState(0);
+  const [viewAllTrans, setViewAllTrans] = useState(true);
   const router = useRouter();
 
   return (
-    <SafeAreaView className="h-full bg-[#1d2128] w-full flex flex-col justify-between">
-      <View className="w-full">
-        <Text className="text-white text-2xl font-bold mt-6 py-6 w-full items-center bg-[#0D1015] text-center">
-          Finances
-        </Text>
+    <SafeAreaView className="h-full bg-[#1d2128] w-full flex flex-col ">
+      <View className={`w-full bg-[#0D1015] mt-6 py-6`}>
+        {viewAllTrans ? (
+          <Text className="text-white text-2xl font-bold w-full items-center  text-center">
+            Finances
+          </Text>
+        ) : (
+          <TouchableOpacity
+            onPress={() => setViewAllTrans(true)}
+            className="w-full flex flex-row items-center gap-6 pl-4"
+          >
+            <ArrowBack />
+            <Text className="text-white text-2xl font-bold">
+              Transaction history
+            </Text>
+          </TouchableOpacity>
+        )}
+      </View>
 
+      {viewAllTrans && (
         <View className="w-full flex flex-row justify-between px-28">
           {["Loans", "Savings"].map((text, i) => (
             <TouchableOpacity
@@ -36,6 +52,8 @@ const Finance = () => {
             </TouchableOpacity>
           ))}
         </View>
+      )}
+      {viewAllTrans && (
         <View className="w-full px-4 mt-4 ">
           <LinearGradient
             colors={["#3C2A07", "#92822E", "#4C4611"]}
@@ -68,20 +86,46 @@ const Finance = () => {
             </TouchableOpacity>
           </LinearGradient>
         </View>
-        <View className="px-4 flex flex-row">
+      )}
+      {viewAllTrans && (
+        <View className="px-4 flex flex-row justify-between mt-4">
           <CustomButton
-            title="Proceed"
+            title="Repay loan"
             onPress={() =>
               router.replace("/(auth)/(member)/(join)/join-stages")
             }
-            className=""
+            className="w-[48%]"
           />
           <TouchableOpacity
-            className={`w-full p-3 mb-5 rounded-full flex flex-row justify-center items-center h-[44px] border border-white`}
+            className={`w-[48%] mb-5 rounded-full  border border-white overflow-hidden`}
             onPress={() => router.replace("/(root)/(tabs)/home")}
           >
-            <Text className="text-white">Cancel</Text>
+            <LinearGradient
+              colors={[
+                "rgba(244, 244, 244, 0.2)",
+                "rgba(255, 255, 255, 0.044)",
+              ]}
+              start={{ x: 0, y: 0.5 }}
+              end={{ x: 1, y: 0.5 }}
+              className="w-full flex flex-row justify-center items-center"
+            >
+              <Text className="text-white text-lg font-bold p-3">
+                Apply for loan
+              </Text>
+            </LinearGradient>
           </TouchableOpacity>
+        </View>
+      )}
+      <View className="px-4 w-full mt-8">
+        <View className="border-b border-gray-50 pb-2 flex flex-row justify-between">
+          <Text className="text-white text-lg font-bold">
+            {viewAllTrans ? "Loan history" : "Loan transactions"}
+          </Text>
+          {viewAllTrans && (
+            <TouchableOpacity onPress={() => setViewAllTrans(false)}>
+              <Text className="text-primary font-bold text-lg">View all</Text>
+            </TouchableOpacity>
+          )}
         </View>
       </View>
     </SafeAreaView>
